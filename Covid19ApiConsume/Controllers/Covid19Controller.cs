443c7955cd02266bdd5dac399f8baa50c6372160
zakariabijoy <zakaria.bijoy@live.com>
2020-05-04 04:04:40 +0600

    Api call  from  mvc controller, get data and show data is done

--- Covid19ApiConsume/Controllers/Covid19Controller.cs
+++ Covid19ApiConsume/Controllers/Covid19Controller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Covid19ApiConsume.Models;
@@ -15,20 +16,26 @@ namespace Covid19ApiConsume.Controllers
         // GET: Covid19
         public ActionResult Index()
         {
-           var covid19 = new Covid19ViewModel();
+            
+            return View();
+        }
+
+        public ActionResult GetCovid19Data()
+        {
+            var covid19 = new Covid19ViewModel();
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://api.covid19api.com/");
 
                 var response =
                     client.GetAsync("summary?fbclid=IwAR0OKbr_6W79uEpZ5HExsnOie2NdJCu7hSev_JQgYV4SM5KiM9A5LVCTyDs").Result;
-                    
 
-                
+
+
                 if (response.IsSuccessStatusCode)
                 {
 
-                   covid19 = response.Content.ReadAsAsync<Covid19ViewModel>().Result;             // install-package Microsoft.AspNet.WebApi.Client is needed for ReadAsAsync<>
+                    covid19 = response.Content.ReadAsAsync<Covid19ViewModel>().Result;             // install-package Microsoft.AspNet.WebApi.Client is needed for ReadAsAsync<>
 
 
                 }
@@ -38,7 +45,7 @@ namespace Covid19ApiConsume.Controllers
                 }
             }
 
-            return View(covid19);
+            return PartialView("_GetCovid19Data",covid19);
         }
     }
 }
